fd95e9f6554b40ce5072e609b504aa7d34b3b1d3
Szymon Wilczek <kazikwilczek7@gmail.com>
2025-10-05 17:30:21 +0200

    nvim config

---
+++ nvim/lua/chadrc.lua
@@ -0,0 +1,54 @@
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+  theme = "bearded-arc",
+  transparency = true,
+
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
+}
+
+M.nvdash = {
+  load_on_startup = true,
+  header = {
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                     ",
+    "       ████ ██████           █████      ██                     ",
+    "      ███████████             █████                             ",
+    "      █████████ ███████████████████ ███   ███████████   ",
+    "     █████████  ███    █████████████ █████ ██████████████   ",
+    "    █████████ ██████████ █████████ █████ █████ ████ █████   ",
+    "  ███████████ ███    ███ █████████ █████ █████ ████ █████  ",
+    " ██████  █████████████████████ ████ █████ █████ ████ ██████ ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+    "                                                                       ",
+  },
+}
+M.ui = {
+  statusline = {
+    theme = "vscode_colored",
+  },
+}
+
+M.lsp = {
+  signature = true
+}
+
+M.colorify = {
+  enabled = true,
+  mode = "virtual",
+  virt_text = "󱓻 ",
+  highlight = { hex = true, lspvars = true },
+}
+
+return M
